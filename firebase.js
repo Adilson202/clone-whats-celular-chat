5e5e4a4a0823000c05f16a78cdb3b7fb282ad942
Adilson202 <adilson@flaro.net>
2022-09-21 07:14:48 -0400

    commit-clone-chat

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABZezTGQzIogMwAmmGy6-KZt28NpLP7bc",
-  authDomain: "whatsapp-clone-82271.firebaseapp.com",
-  projectId: "whatsapp-clone-82271",
-  storageBucket: "whatsapp-clone-82271.appspot.com",
-  messagingSenderId: "832888188143",
-  appId: "1:832888188143:web:058e5fe2f98c0798f6ff1a"
+  apiKey: "AIzaSyA0xYx4iOM7FfEmMJt_3C_Tlj9rM8Wf6fk",
+  authDomain: "whatsapp-com-chat.firebaseapp.com",
+  projectId: "whatsapp-com-chat",
+  storageBucket: "whatsapp-com-chat.appspot.com",
+  messagingSenderId: "181166821193",
+  appId: "1:181166821193:web:b769afe2c07390b2f65498"
 };
 
 const app = !firebase.apps.length
